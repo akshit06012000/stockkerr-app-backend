52a5e45e3de11bd30a167a7969096ad4bff3893b
akshit06012000 <96385266+akshit06012000@users.noreply.github.com>
2023-01-01 23:16:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,13 +25,14 @@ app.use(bodyParser.json());
   })
 );*/
 
-app.use(function (req, res, next) {
-  //Enabling CORS
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type")
-  });
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8000/');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+};
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Routes Middleware
